71dd947d39117a2bc4943584209080346129d88c
Shoham Peller <shohamp@gmail.com>
2023-09-14 14:36:33 +0300

    isDebuggable = false in release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isDebuggable = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
