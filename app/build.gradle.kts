1a7499d716d6af893ae2b701b5c7e956043cd1c8
Shoham Peller <shohamp@gmail.com>
2023-09-14 19:37:49 +0300

    New version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.clock.clock_screensaver"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
